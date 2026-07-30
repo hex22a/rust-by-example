92073db88e795ce131c34a9a4c89955f8b99cffa
Crash <1903291+hex22a@users.noreply.github.com>
2026-07-30 15:02:04 +0400

    display

---
+++ src/display.rs
@@ -0,0 +1,87 @@
+use std::fmt; // Import `fmt`
+
+// A structure holding two numbers. `Debug` will be derived so the results can
+// be contrasted with `Display`.
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+// Implement `Display` for `MinMax`.
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Use `self.number` to refer to each positional data point.
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+// Define a structure where the fields are nameable for comparison.
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+// Similarly, implement `Display` for `Point2D`.
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl fmt::Display for Complex {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let sign = if self.imag < 0.0 { '-' } else { '+' };
+        write!(f, "{}  {} {}i", self.real, sign, self.imag.abs())
+    }
+}
+
+pub fn run() {
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures:");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!(
+        "The big range is {big} and the small is {small}",
+        small = small_range,
+        big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points:");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    // The following line would not compile: both `Debug` and `Display`
+    // were implemented, but `{:b}` requires `fmt::Binary` to be
+    // implemented, which it hasn't been for `Point2D`.
+    // println!("What does Point2D look like in binary: {:b}?", point);
+
+    let c1 = Complex {
+        real: 3.3,
+        imag: 7.2,
+    };
+    let c2 = Complex {
+        real: 4.7,
+        imag: -2.3,
+    };
+
+    println!();
+    println!("Complex numbers:");
+    println!("Display: {}", c1);
+    println!("Debug: {:?}", c1);
+    println!();
+    println!("Display: {}", c2);
+    println!("Debug: {:?}", c2);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+mod display;
 mod formatted_print;
 mod hello_world;
 
 fn main() {
     hello_world::run();
+    println!();
     formatted_print::run();
+    println!();
+    display::run();
+    println!();
 }
