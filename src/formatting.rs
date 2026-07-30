4acce9e7b2561d924802a41660f780f4d678efb3
Crash <1903291+hex22a@users.noreply.github.com>
2026-07-30 16:14:01 +0400

    formatting hex

--- src/formatting.rs
+++ src/formatting.rs
@@ -39,7 +39,7 @@ impl Display for Color {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "RGB ({red}, {green}, {blue})",
+            "RGB ({red}, {green}, {blue}) 0x{red:02X}{green:02X}{blue:02X}",
             red = self.red,
             green = self.green,
             blue = self.blue
